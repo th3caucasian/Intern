e4aa0de9faf168d7eb4697aa0698143216d3a35a
th3caucasian <alan-erkenov@mail.ru>
2025-02-04 12:33:50 +0300

    Почистил проект

--- Intern/ViewController.swift
+++ Intern/ViewController.swift
@@ -8,12 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // Создаём кнопку
+    let myButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Нажми меня", for: .normal)
+        button.backgroundColor = .blue
+        button.setTitleColor(.white, for: .normal)
+        button.layer.cornerRadius = 10
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        
+        // Добавляем кнопку на экран
+        view.addSubview(myButton)
+        
+        // Устанавливаем ограничения
+        myButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            myButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            myButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            myButton.widthAnchor.constraint(equalToConstant: 200),
+            myButton.heightAnchor.constraint(equalToConstant: 50)
+        ])
     }
-
-
 }
+
 
