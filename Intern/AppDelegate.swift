61d36b66975619b38a36004df239aa0a7c256c60
Erkenov Alan <alan-erkenov@mail.ru>
2025-02-06 16:12:02 +0300

    Добавил Nav Controller

--- Intern/AppDelegate.swift
+++ Intern/AppDelegate.swift
@@ -14,8 +14,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        let navigationController = UINavigationController(rootViewController: ViewController())
         window = UIWindow(frame: UIScreen.main.bounds)
-        window?.rootViewController = MyTableViewController()
+        window?.rootViewController = navigationController
         window?.makeKeyAndVisible()
         return true
     }
--- Intern/Controllers/ViewController.swift
+++ Intern/Controllers/ViewController.swift
@@ -13,11 +13,36 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemGray5
+        self.title = "Главный экран"
+        if let navigationBar = self.navigationController?.navigationBar {
+            if #available(iOS 13.0, *) {
+                let appearance = UINavigationBarAppearance()
+                appearance.configureWithOpaqueBackground()
+                appearance.backgroundColor = .lightBlue
+                appearance.titleTextAttributes = [
+                    .font: UIFont.systemFont(ofSize: 22, weight: .semibold),
+                    .foregroundColor: UIColor.white
+                ]
+                navigationBar.scrollEdgeAppearance = appearance
+                navigationBar.standardAppearance = appearance
+                navigationBar.compactAppearance = appearance
+            }
+            else {
+                navigationBar.backgroundColor = .lightBlue
+                navigationBar.titleTextAttributes = [
+                    .font: UIFont.systemFont(ofSize: 22, weight: .semibold),
+                    .foregroundColor: UIColor.white
+                ]
+            }
+        }
+        
         let card = Card(frame: UIScreen.main.bounds)
         let stack = CardStack(frame: UIScreen.main.bounds)
         view.addSubview(stack)
-        stack.edgesToSuperview(insets: TinyEdgeInsets(top: 30, left: 0, bottom: 30, right: 0),usingSafeArea: true)
+        stack.edgesToSuperview(insets: TinyEdgeInsets(top: 30, left: 0, bottom: 30, right: 0), usingSafeArea: true)
         stack.setupView()
+        
+
     }
 }
 
