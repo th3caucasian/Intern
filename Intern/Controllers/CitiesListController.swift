b1bd547efef90245921f312b97ee5225fc42c285
Erkenov Alan <alan-erkenov@mail.ru>
2025-02-07 15:27:38 +0300

    Изменил архитектуру списка ListView (добавил наследуемые классы)

---
+++ Intern/Controllers/CitiesListController.swift
@@ -0,0 +1,7 @@
+//
+//  Untitled.swift
+//  Intern
+//
+//  Created by Алан Эркенов on 07.02.2025.
+//
+
---
+++ Intern/Controllers/CryptoListController.swift
@@ -0,0 +1,8 @@
+//
+//  CryptoListController.swift
+//  Intern
+//
+//  Created by Алан Эркенов on 07.02.2025.
+//
+
+import Foundation
